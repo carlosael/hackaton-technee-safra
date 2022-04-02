3340930155d76beecf5c1beaa4ecc820b294bd84
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-04-02 18:33:27 -0300

    feat: footer created

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,1 +1,69 @@
+import "./style.css";
+import LinkedinIcon from "../../assets/icon-linkedin.svg";
+import InstagramIcon from "../../assets/instagram-safra.svg";
+import YoutubeIcon from "../../assets/Youtube.svg";
+import FacebookIcon from "../../assets/icon-facebook.svg";
+import TelegramIcon from "../../assets/telegram.svg";
+import TwitterIcon from "../../assets/twitter.svg";
 
+export default function Footer() {
+  return (
+    <footer>
+      <div class="links">
+        <div class="logo-nav">
+          <ul>
+            <li>
+              <a href="#">Serviços</a>
+            </li>
+            <li>
+              <a href="#">Relacionamento</a>
+            </li>
+            <li>
+              <a href="#">Sobre</a>
+            </li>
+            <li>
+              <a href="#">Atendimento</a>
+            </li>
+            <li>
+              <a href="#">Investimentos</a>
+            </li>
+          </ul>
+        </div>
+        <div class="ul-social">
+          <ul>
+            <li>
+              <a href="https://www.linkedin.com/company/safra-banco/?originalSubdomain=br">
+                <img src={LinkedinIcon} alt="Linkedin Icon"></img>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/bancosafra/?hl=pt-br">
+                <img src={InstagramIcon} alt="Linkedin Icon"></img>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/channel/UC_eA5WnJz2rQ2iJ9cF97adw">
+                <img src={YoutubeIcon} alt="Linkedin Icon"></img>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/safrainvestimentos">
+                <img src={FacebookIcon} alt="Linkedin Icon"></img>
+              </a>
+            </li>
+            <li>
+              <a href="https://t.me/safraconteudo">
+                <img src={TelegramIcon} alt="Linkedin Icon"></img>
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/bancosafra">
+                <img src={TwitterIcon} alt="Linkedin Icon"></img>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
