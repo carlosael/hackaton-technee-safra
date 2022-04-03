f934817207e5d5154bff6213a4f87c4a5b3635cf
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-04-03 17:27:27 -0300

    fix: deploy fix

--- src/services/getBankDetails.js
+++ src/services/getBankDetails.js
@@ -1,3 +1,5 @@
+import DeniedToast from "../helpers/Toastify/Toastify";
+
 async function getBankDetails(setLoanTypes) {
   try {
     const response = await fetch("https://api.safra.com.br/open-banking/products-services/v1/business-loans", {
@@ -10,7 +12,7 @@ async function getBankDetails(setLoanTypes) {
 
     setLoanTypes(data.data.brand.companies[0].businessLoans);
   } catch (error) {
-    console.log(error);
+    DeniedToast(error.message);
   }
 }
 
